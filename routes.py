f3f2f42e58c1485125ea18d6ba1e67068e3f9d09
William <wmayhood@outlook.com>
2025-01-21 14:40:55 -0500

    cors error fixed

--- routes.py
+++ routes.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 from controllers import makePrediction
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-
+CORS(app)
 @app.route('/getPrediction', methods=['POST'])
 
 def predict():
